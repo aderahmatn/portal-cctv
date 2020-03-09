05e85ef30151690995ffae8978299574b99945ad
ade rahmat nurdiyana <nurdiyana.ade@gmail.com>
2020-03-09 18:32:56 +0700

    add login-page

--- application/views/layout.php
+++ application/views/layout.php
@@ -41,184 +41,189 @@
       <hr class="sidebar-divider my-0">
 
       <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
-        <a class="nav-link" href="#">
-          <i class="fad fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Administrasi
-      </div>
-		<li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fad fa-stream"></i>
-          <span>Catatan Aktivitas</span></a>
-      </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
-        	<i class="far fa-cctv"></i>
-          <span>Laporan Temuan</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fad fa-toolbox"></i>
-          <span>Laporan Perbaikan</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fad fa-eye"></i>
-          <span>Catatan Edit Absen</span></a>
-      </li>
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Data Master
-      </div>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fad fa-map-marked-alt"></i>
-          <span>CCTV Area</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fad fa-network-wired"></i>
-          <span>IP Addres</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-      		<i class="fad fa-users"></i>
-          <span>Staff CCTV</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fad fa-users-class"></i>
-          <span>PIC Temuan</span></a>
-      </li>
-      <!-- Divider -->
-      <hr class="sidebar-divider d-none d-md-block">
-    </ul>
-    <!-- End of Sidebar -->
+        <a class="nav-link" href="#" disabled>
+          <span><small><?= strtoupper(hari_ini().' - '.date('d/m/Y'))?></small></span></a>
+        </li>
 
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
-
-      <!-- Main Content -->
-      <div id="content">
-
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-gray-100 topbar mb-4 static-top shadow-lg">
-
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-
-          
-
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-            <div class="topbar-divider d-none d-sm-block text-gray-900"></div>
-            <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-900 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-              </a>
-              <!-- Dropdown - User Information -->
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
-                </a>
-              </div>
-            </li>
-
-          </ul>
-
-        </nav>
-        <!-- End of Topbar -->
-
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
-
-          <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
-
-        </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
-
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <a href="https://github.com/aderahmatn">ADRN</a> <?=date('Y') ?></span>
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            <i class="fad fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+          </li>
+          <!-- Divider -->
+          <hr class="sidebar-divider">
+          <!-- Heading -->
+          <div class="sidebar-heading">
+            Administrasi
           </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="fad fa-stream"></i>
+              <span>Catatan Aktivitas</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+               <i class="far fa-cctv"></i>
+               <span>Laporan Temuan</span></a>
+             </li>
+             <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fad fa-toolbox"></i>
+                <span>Laporan Perbaikan</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <i class="fad fa-eye"></i>
+                  <span>Catatan Edit Absen</span></a>
+                </li>
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                  Data Master
+                </div>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <i class="fad fa-map-marked-alt"></i>
+                    <span>CCTV Area</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      <i class="fad fa-network-wired"></i>
+                      <span>IP Addres</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <i class="fad fa-users"></i>
+                        <span>Staff CCTV</span></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                          <i class="fad fa-users-class"></i>
+                          <span>PIC Temuan</span></a>
+                        </li>
+                        <!-- Divider -->
+                        <hr class="sidebar-divider d-none d-md-block">
+                      </ul>
+                      <!-- End of Sidebar -->
 
-    </div>
-    <!-- End of Content Wrapper -->
+                      <!-- Content Wrapper -->
+                      <div id="content-wrapper" class="d-flex flex-column">
 
-  </div>
-  <!-- End of Page Wrapper -->
+                        <!-- Main Content -->
+                        <div id="content">
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
+                          <!-- Topbar -->
+                          <nav class="navbar navbar-expand navbar-light bg-gray-100 topbar mb-4 static-top shadow-lg">
 
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
+                            <!-- Sidebar Toggle (Topbar) -->
+                            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                              <i class="fa fa-bars"></i>
+                            </button>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?=base_url().'assets/vendor/jquery/jquery.min.js'?>"></script>
-  <script src="<?=base_url().'assets/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="<?=base_url().'assets/vendor/jquery-easing/jquery.easing.min.js'?>"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="<?=base_url().'assets/js/sb-admin-2.min.js'?>"></script>
+                            <!-- Topbar Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                              <div class="topbar-divider d-none d-sm-block text-gray-900"></div>
+                              <!-- Nav Item - User Information -->
+                              <li class="nav-item dropdown no-arrow">
+                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  <span class="mr-2 d-none d-lg-inline text-gray-900 small">Valerie Luna</span>
+                                  <img class="img-profile rounded-circle" src="https://avatars2.githubusercontent.com/u/19869153?s=460&v=4">
+                                </a>
+                                <!-- Dropdown - User Information -->
+                                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                  <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                  </a>
+                                  <a class="dropdown-item" href="#">
+                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Settings
+                                  </a>
+                                  <a class="dropdown-item" href="#">
+                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Activity Log
+                                  </a>
+                                  <div class="dropdown-divider"></div>
+                                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                  </a>
+                                </div>
+                              </li>
 
-</body>
+                            </ul>
 
-</html>
+                          </nav>
+                          <!-- End of Topbar -->
+
+                          <!-- Begin Page Content -->
+                          <div class="container-fluid">
+
+                            <!-- Page Heading -->
+                            <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                          </div>
+                          <!-- /.container-fluid -->
+
+                        </div>
+                        <!-- End of Main Content -->
+
+                        <!-- Footer -->
+                        <footer class="sticky-footer bg-white">
+                          <div class="container my-auto">
+                            <div class="copyright text-center my-auto">
+                              <span>Copyright &copy; Portal CCTV v.2.0 - <a href="https://github.com/aderahmatn">ADRN</a> <?=date('Y') ?></span>
+                            </div>
+                          </div>
+                        </footer>
+                        <!-- End of Footer -->
+
+                      </div>
+                      <!-- End of Content Wrapper -->
+
+                    </div>
+                    <!-- End of Page Wrapper -->
+
+                    <!-- Scroll to Top Button-->
+                    <a class="scroll-to-top rounded" href="#page-top">
+                      <i class="fas fa-angle-up"></i>
+                    </a>
+
+                    <!-- Logout Modal-->
+                    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">×</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                          <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                            <a class="btn btn-primary" href="login.html">Logout</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Bootstrap core JavaScript-->
+                    <script src="<?=base_url().'assets/vendor/jquery/jquery.min.js'?>"></script>
+                    <script src="<?=base_url().'assets/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+
+                    <!-- Core plugin JavaScript-->
+                    <script src="<?=base_url().'assets/vendor/jquery-easing/jquery.easing.min.js'?>"></script>
+
+                    <!-- Custom scripts for all pages-->
+                    <script src="<?=base_url().'assets/js/sb-admin-2.min.js'?>"></script>
+
+                  </body>
+
+                  </html>
---
+++ application/views/login.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Portal CCTV - Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="<?=base_url().'assets/vendor/fontawesome/css/all.min.css'?>" rel="stylesheet" type="text/css">
+  <link href="<?=base_url().'assets/css/font.css'?>" rel="stylesheet">
+
+
+  <!-- Custom styles for this template-->
+  <link href="<?=base_url().'assets/css/sb-admin-2.css'?>" rel="stylesheet">
+
+</head>
+
+<body class="bg-gray-900">
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-5 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h1 text-gray-900 mb-2"><i class="fad fa-cctv"></i></61>
+                    <h1 class="h4 text-gray-900 mb-4"><strong>PORTAL CCTV</strong></h1>
+                  </div>
+                  <form class="user" action="../function/act_login.php" method="post">
+                    <div class="form-group">
+                      <input type="text" class="form-control form-control-user" name="funame" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Username" autocomplete="off" autofocus>
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" name="fpass" placeholder="Password" autocomplete="off">
+                    </div>
+                    <button class="btn btn-secondary btn-user btn-block mb-4" type="submit">LOGIN</button>
+                    
+                    <hr>
+                    <div class="text-center">
+                      <p class="text-gray-400 text-xs">Copyright &copy; Portal CCTV v.2.0 - <a href="https://github.com/aderahmatn" target="_blank">ADRN</a> <?=date('Y') ?></p>
+                  </div>
+                  </form>
+                  <?php 
+                  if(isset($_GET['pesan'])){
+                    if($_GET['pesan'] == "gagal"){
+                      echo '<div class="alert alert-danger">
+                      Username atau Password salah! <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                      </button> </div>';
+                    }else if($_GET['pesan'] == "logout"){
+                      echo '<div class="alert alert-success">
+                      Anda sudah Log Out <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                      </button> </div>';
+                    }else if($_GET['pesan'] == "logindulu"){
+                      echo '<div class="alert alert-danger">
+                      Anda harus login terlebih dahulu! <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                      </button> </div>';
+                    }
+                  }
+                  ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="<?=base_url().'assets/vendor/jquery/jquery.min.js'?>"></script>
+  <script src="<?=base_url().'assets/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?=base_url().'assets/vendor/jquery-easing/jquery.easing.min.js'?>"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?=base_url().'assets/js/sb-admin-2.min.js'?>"></script>
+
+</body>
+
+</html>
